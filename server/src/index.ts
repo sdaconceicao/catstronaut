4e6c542c4e98c474fe82fddc5ac1dd8104b04934
Stephen da Conceicao <daconcei472@gmail.com>
2025-06-02 23:14:17 -0400

    feat: setup mocks

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { typeDefs } from "./schema";
+import { mocks } from "./mock";
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+      🚀  Server is running!
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
---
+++ server/src/mock.ts
@@ -0,0 +1,20 @@
+export const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(6)],
+  }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
